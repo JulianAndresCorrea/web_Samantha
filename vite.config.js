42975ac72538de80cdbc3f1c34176086e8090c9d
JulianAndresCorrea <juliancorream.dev@gmail.com>
2026-05-02 17:35:14 -0500

    configurar despliegue automatico a github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANTE PARA GITHUB PAGES:
+  // Si tu repositorio se llama "safiro-nails", debes descomentar la línea de abajo 
+  // y poner el nombre exacto de tu repositorio entre las barras.
+  base: '/web_Samantha/',
 })
